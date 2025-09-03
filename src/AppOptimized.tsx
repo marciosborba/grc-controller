cf719c93c1a916e1a070055283a3f26cc90c8c2c
Marcio Borba <adm@grc-controller.com>
2025-09-03 14:21:47 -0300

    Melhoria na UI 12

--- src/AppOptimized.tsx
+++ src/AppOptimized.tsx
@@ -129,7 +129,7 @@ const App = () => (
             <TooltipProvider>
               <Toaster />
               <Sonner />
-              <BrowserRouter>
+              <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
               <Routes>
                 {/* Public Routes - Carregamento imediato */}
                 <Route path="/login" element={
